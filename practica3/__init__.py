c0f4634817c29dee09165869cc99dcf9941feba0
daniL16 <dani16lp@gmail.com>
2016-11-03 13:27:29 +0100

    practica3 50%

--- practica3/__init__.py
+++ practica3/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, session, redirect,render_template
 from flask import url_for, escape, request
+import shelve
+import re
 
 app = Flask(__name__)
-app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
+shelve.open('shelve.db')
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 
 def ultima():
     try:
@@ -10,11 +13,44 @@ def ultima():
         return session['ultima']
     except KeyError:
         session['ultima'] = 0
-@app.route('/index')
+        
+
+@app.route("/")
 def index():
+    if "username" in session:
+        return render_template("index_logged.html", username=session["username"])
+    else:
+        return render_template("index.html")
+
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
+
+@app.route("/usuario", methods=["POST"])
+def nuevo_usuario():
+    username = request.form["username"]
+    if re.match(r"^([a-zA-Z0-9]+)$", username):
+        db = shelve.get_shelve('c')
+        if "users:{}".format(username) not in db:
+            db["users:{}".format(username)] = {
+                "username": username,
+                "password": request.form["password"],
+                "realname": request.form["realname"],
+                "email":    request.form["email"]
+            }
+            return redirect(url_for("index"))
+        else:
+            return redirect(url_for("registro"))
+    else:
+        return redirect(url_for("registro"))
+        return redirect(url_for("registro"))
+
+
+@app.route('/page1')
+def page1():
     ult = ultima()
-    session[ult]='index.html'
-    return render_template('index.html')
+    session[ult]='page1.html'
+    return render_template('page1.html')
 
 @app.route('/page2')
 def page2():
@@ -22,13 +58,7 @@ def page2():
     session[ult]='page2.html'
     return render_template('page2.html')
 
-@app.route('/page3')
-def page3():
-    ult = ultima()
-    session[ult]='page3.html'
-    return render_template('page3.html')
-
-@app.route('/',methods=["POST"])
+@app.route('/login',methods=["POST"])
 def login():
     if request.method== 'POST':
         session['username'] = request.form['username']
@@ -36,7 +66,7 @@ def login():
     
 @app.route('/logout')
 def logout():
-    session.pop('username', None)
+    session.clear()
     return redirect(url_for('index'))
 
 
