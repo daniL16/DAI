bf7ae7e1035c7e85d7aa5596c38aec8ab6fb478e
daniL16 <dani16lp@gmail.com>
2016-11-03 18:53:58 +0100

    practica3 75%

--- practica3/__init__.py
+++ practica3/__init__.py
@@ -1,25 +1,17 @@
+import flask
 from flask import Flask, session, redirect,render_template
 from flask import url_for, escape, request
 import shelve
 import re
+from flask_shelve import init_app
 
-app = Flask(__name__)
-shelve.open('shelve.db')
+app = flask.Flask(__name__)
+app.config['SHELVE_FILENAME'] = 'shelve.db'
+init_app(app)
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
-
-def ultima():
-    try:
-        session['ultima'] = (session['ultima']+1)%3
-        return session['ultima']
-    except KeyError:
-        session['ultima'] = 0
-        
 
 @app.route("/")
 def index():
-    if "username" in session:
-        return render_template("index_logged.html", username=session["username"])
-    else:
         return render_template("index.html")
 
 @app.route("/registro")
@@ -30,20 +22,36 @@ def registro():
 def nuevo_usuario():
     username = request.form["username"]
     if re.match(r"^([a-zA-Z0-9]+)$", username):
-        db = shelve.get_shelve('c')
-        if "users:{}".format(username) not in db:
+        db = shelve.open('shelve.db')
+        if db.has_key("users:{}".format(username)):
+            return redirect(url_for("registro"))
+            
+        else:
             db["users:{}".format(username)] = {
                 "username": username,
                 "password": request.form["password"],
-                "realname": request.form["realname"],
+                "name": request.form["name"],
                 "email":    request.form["email"]
             }
             return redirect(url_for("index"))
-        else:
-            return redirect(url_for("registro"))
     else:
         return redirect(url_for("registro"))
-        return redirect(url_for("registro"))
+    
+@app.route('/login',methods=["POST"])
+def login():
+    username = request.form["username"]
+    db = shelve.open('shelve.db')
+    key = "users:{}".format(username)
+    if db.has_key(key) and db[key]["password"] == request.form["password"]:
+        session["username"] = request.form["username"]
+        session["history"] = []
+        
+    return redirect(url_for("index"))
+    
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
 
 
 @app.route('/page1')
@@ -58,18 +66,30 @@ def page2():
     session[ult]='page2.html'
     return render_template('page2.html')
 
-@app.route('/login',methods=["POST"])
-def login():
-    if request.method== 'POST':
-        session['username'] = request.form['username']
-        return render_template('index.html')
     
-@app.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('index'))
+@app.route('/usuario/datos')
+def usuario():
+    db = shelve.open('shelve.db')
+    key = "users:{}".format(session["username"])
+    user = db[key]
+    return render_template("usuario.html", user=user)
 
-
+@app.route('/usuario/modificar',methods=["POST"])
+def modificar_usuario():
+    db = shelve.open('shelve.db')
+    username = session["username"]
+    if request.method=="POST":
+        db["users:{}".format(username)] = {
+                "username": username,
+                "password": request.form["pass"],
+                "name": request.form["name"],
+                "email": request.form["email"]
+            }
+        return redirect(url_for("index"))
+    else:
+        key = "users:{}".format(username)
+        user = db[key]
+        return render_template("moficiar_usuario.html", user=user)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug='True')
