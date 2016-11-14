9643897a13b259244c05db6fa59877996a750b04
daniL16 <dani16lp@gmail.com>
2016-11-14 13:12:47 +0100

    inicio p4

---
+++ practica4/__init__.py
@@ -0,0 +1,130 @@
+import flask
+from flask import Flask, session, redirect,render_template
+from flask import url_for, escape, request
+import shelve
+import re
+from flask_shelve import init_app
+from collections import deque
+import pymongo
+
+app = flask.Flask(__name__)
+app.config['SHELVE_FILENAME'] = 'shelve.db'
+init_app(app)
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+@app.route("/")
+def index():
+        return render_template("index.html")
+
+@app.route("/registro")
+def registro():
+    return render_template("registro.html")
+
+@app.route("/usuario", methods=["POST"])
+def nuevo_usuario():
+    username = request.form["username"]
+    if re.match(r"^([a-zA-Z0-9]+)$", username):
+        db = shelve.open('shelve.db')
+        if db.has_key("users:{}".format(username)):
+            return redirect(url_for("registro"))
+            
+        else:
+            db["users:{}".format(username)] = {
+                "username": username,
+                "password": request.form["password"],
+                "name": request.form["name"],
+                "email":    request.form["email"]
+            }
+            return redirect(url_for("index"))
+    else:
+        return redirect(url_for("registro"))
+    
+@app.route('/login',methods=["POST"])
+def login():
+    username = request.form["username"]
+    db = shelve.open('shelve.db')
+    key = "users:{}".format(username)
+    if db.has_key(key) and db[key]["password"] == request.form["password"]:
+        session["username"] = request.form["username"]
+        session["history"] = []
+        
+    return redirect(url_for("index"))
+    
+@app.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+@app.route('/page1')
+def page1():
+    return render_template('page1.html')
+
+@app.route('/page2')
+def page2():
+    return render_template('page2.html',title=' Elementos flotantes y clearing')
+
+    
+@app.route('/usuario/datos')
+def usuario():
+    db = shelve.open('shelve.db')
+    key = "users:{}".format(session["username"])
+    user = db[key]
+    return render_template("usuario.html", user=user)
+
+@app.route('/usuario/modificar',methods=["GET","POST"])
+def modificar_usuario():
+    db = shelve.open('shelve.db')
+    username = session["username"]
+    if request.method=="POST":
+        db["users:{}".format(username)] = {
+                "username": username,
+                "password": request.form["pass"],
+                "name": request.form["name"],
+                "email": request.form["email"]
+            }
+        return redirect(url_for("index"))
+    else:
+        key = "users:{}".format(username)
+        user = db[key]
+        return render_template("modificar_usuario.html", user=user)
+
+@app.after_request
+def save_history(response):
+    if "username" in session and response.mimetype == "text/html":
+            h = deque(session["history"], 3)
+            h.appendleft(request.path)
+            session["history"] = list(h)
+    return response
+
+@app.route("/nuevo_restaurante", methods=["GET","POST"])
+def nuevo_restaurante():
+    if request.method=="POST":
+        address = {"building":request.form["building"],
+                  "street":request.form["street"],
+                  "zipcode":request.form["zipcode"],
+                  "coord":"["+request.form["coord1"]+","+request.form["coord2"]+"]"
+                 }
+        rest = {"name":request.form["name"],
+            "borough":request.form["borough"],
+            "cuisine":request.form["cuisine"],
+            "address": address,
+            "rest_id": request.form["rest_id"]
+           }
+        print rest
+        collection.insert(rest)
+        return redirect(url_for("index"))
+    else:
+        return render_template("nuevo_restaurante.html")
+
+if __name__ == '__main__':
+    try:
+        conn=pymongo.MongoClient()
+        db = conn.p4
+        collection = db.restaurants
+        print "Connected successfully!!!"
+    except pymongo.errors.ConnectionFailure, e:
+        print "Could not connect to MongoDB: %s" % e 
+    print collection
+    app.run(host='0.0.0.0',debug='True')
+    
