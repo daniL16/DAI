960bdd43dbf36a292f5b95aa2e98aca82d24c8ed
daniL16 <dani16lp@gmail.com>
2017-07-09 18:19:27 +0200

    p5.1

--- web/static/js/p5.js
+++ web/static/js/p5.js
@@ -1,20 +1,30 @@
-
 $(document).ready(function(){
-  $(".aumentarFuente").click(function(){
+    $("#submenu_rest").hide();
+     $("#submenu_users").hide();
+    $("#rest").hover(function(e){
+        $("#submenu_rest").toggle(300,stop);
+    });
+     $("#users").hover(function(e){
+        $("#submenu_users").toggle(300,stop);
+    });
+});
+
+
+function aumentarFuente() {
   	var fuente = $('body').css('font-size');
     var act = parseFloat(fuente, 10);
 	$('body').css('font-size', act+0.5);
 	return false;
-  });
+  }
  
-  $(".disminuirFuente").click(function(){
+  function disminuirFuente(){
   	var fuente = $('body').css('font-size');
         var act = parseFloat(fuente, 10);
 	$('body').css('font-size', act-0.5);
 	return false;
-  });
+  }
   
-  $(".cambiarColor").click(function(){
+  function cambiarColor(){
 	if ($('body').css('color') == 'rgb(255, 255, 255)'){
   		$('body').css('color','black');
 		$('body').css('background','white');
@@ -28,42 +38,4 @@ $(document).ready(function(){
 		$('footer').css('background','black');
 	}
 	return false;
-});
-    
-  $('#submenu').hide();
-    
-  
-  $('#rest').hover(function(e) {
-       $('#submenu').toggle(300,stop);
-     });
-
-
-    function paginador(page){
-    var pagina_actual=page;
-
- $.ajax({
-                url: "/quiero_filas_desde_la",
-                data: { fila: pagina_actual*10},
-                type: 'get',                        
-                success: function(datos) {
-                        Visualiza_filas (datos);  
-                },
-                failure: function(datos) {
-                        alert('esto no vá');
-                }
-        });
 }
-
-
-
-});
-
-
-function Visualiza_datos (datos) {
-    console.log(lista_articulos);
-    var $contendor_principal = $('#resultado_busqueda') ;
-}
-   
-$('#page2').on('click', function(e){
-   paginador(2);
-});
